3b44c87af54f9b0c67dd674143a8899b36064487
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-01-23 19:52:57 +0100

    Finished

--- script.js
+++ script.js
@@ -48,49 +48,51 @@ function counterManager() {
     counterData = data;
   };
 
-  return { addData, getData, setData };
+  const calculateMarblesNum = () => {
+    const totalMarbles = counterData.reduce((sum, data) => {
+      return sum + data.getCurrentNum();
+    }, 0);
+
+    return totalMarbles;
+  };
+
+  const removeCounter = (id) => {
+    const updatedCounters = manager
+      .getData()
+      .filter((data) => data.getId() !== id);
+    manager.setData(updatedCounters);
+    manager.calculateMarblesNum();
+  };
+
+  return { addData, getData, setData, calculateMarblesNum, removeCounter };
 }
 
 const manager = counterManager();
 
-function calculateMarblesNum() {
-  const totalMarbles = manager.getData().reduce((sum, data) => {
-    return sum + data.getCurrentNum();
-  }, 0);
+boxList.addEventListener("click", (e) => {
+  const counterElement = e.target.closest("li");
+  if (!counterElement) return;
 
-  marbles.textContent = totalMarbles;
-}
+  const id = counterElement.id;
+  const data = manager.getData().find((data) => data.getId() === id);
 
-function removeCounter(id) {
-  const updatedCounters = manager
-    .getData()
-    .filter((data) => data.getId() !== id);
-  manager.setData(updatedCounters);
-  calculateMarblesNum();
-}
-
-function getCounterButtons(counterElement, data) {
-  const removeBtn = counterElement.querySelector(".removeBtn");
-  const minusBtn = counterElement.querySelector(".minusBtn");
-  const plusBtn = counterElement.querySelector(".plusBtn");
   const spanNumber = counterElement.querySelector(".spanNumber");
 
-  removeBtn.addEventListener("click", () => {
-    counterElement.remove();
-    removeCounter(data.getId());
-    boxes.textContent = manager.getData().length;
-  });
-
-  plusBtn.addEventListener("click", () => {
+  if (e.target.closest(".plusBtn")) {
     spanNumber.textContent = data.increaseCounterNum();
-    calculateMarblesNum();
-  });
-
-  minusBtn.addEventListener("click", () => {
+    marbles.textContent = manager.calculateMarblesNum();
+  }
+  if (e.target.closest(".minusBtn")) {
     spanNumber.textContent = data.decreaseCounterNum();
-    calculateMarblesNum();
-  });
-}
+    marbles.textContent = manager.calculateMarblesNum();
+  }
+  if (e.target.closest(".removeBtn")) {
+    counterElement.remove();
+    manager.removeCounter(id);
+    boxes.textContent = manager.getData().length;
+    marbles.textContent = manager.calculateMarblesNum();
+  }
+});
 
 function getCounter(id) {
   const counter = document.createElement("li");
@@ -101,7 +103,7 @@ function getCounter(id) {
     <span class="spanNumber">0</span>
     <button class="plusBtn">+</button>
     <button class="removeBtn">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="trash">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="trash ">
         <path fill-rule="evenodd" d="M8.75 1A2.75 2.75 0 0 0 6 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 1 0 .23 1.482l.149-.022.841 10.518A2.75 2.75 0 0 0 7.596 19h4.807a2.75 2.75 0 0 0 2.742-2.53l.841-10.52.149.023a.75.75 0 0 0 .23-1.482A41.03 41.03 0 0 0 14 4.193V3.75A2.75 2.75 0 0 0 11.25 1h-2.5ZM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4ZM8.58 7.72a.75.75 0 0 0-1.5.06l.3 7.5a.75.75 0 1 0 1.5-.06l-.3-7.5Zm4.34.06a.75.75 0 1 0-1.5-.06l-.3 7.5a.75.75 0 1 0 1.5.06l.3-7.5Z" clip-rule="evenodd" />
       </svg>
     </button>`;
@@ -114,8 +116,6 @@ boxBtn.addEventListener("click", () => {
 
   const counterElement = getCounter(data.getId());
   boxList.appendChild(counterElement);
-
-  getCounterButtons(counterElement, data);
 
   boxes.textContent = manager.getData().length;
 });
